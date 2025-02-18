777381422ef24162ac4444383de8a3f0c545ae9d
Theusm4 <91488182+Theusm4@users.noreply.github.com>
2025-02-18 00:07:21 +0000

    ajustes de codificação e arquivos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     int cod_cidade=0;
-    char nome_cidade[20]='cidade';
+    char nome_cidade[20]="cidade";
     int populacao=1000;
     double area=1750.32;
     double pib=1548321.90;
@@ -19,31 +19,32 @@ int main() {
     printf("Cadastramento de cartas:\n");
 
     printf("Digite o código da cidade:\n");
-    scanf("%d", cod_cidade);
+    scanf("%d", &cod_cidade);
 
     printf("Digite o nome da cidade:\n");
-    scanf("%s", nome_cidade);
+    scanf("%s", &nome_cidade);
 
     printf("Digite quantidade populacional da cidade:\n");
-    scanf("%i", populacao);
+    scanf("%i", &populacao);
 
-    printf("Digite a área total da cidade:\n");
-    scanf("%.2lf", area);
+    printf("Digite a área² total da cidade:\n");
+    scanf("%lf", &area);
 
     printf("Digite o PIB da cidade:\n");
-    scanf("%.2lf", pib);
+    scanf("%lf", &pib);
 
     printf("Digite a quantidade de pontos turísticos:\n");
-    scanf("%i", pts_turisticos);
+    scanf("%i", &pts_turisticos);
 
     // Exibição dos Dados das Cartas:
     printf("\nCarta cadastrada com sucesso, verifique os dados:\n");
 
-    printf("Nome da cidade:\n", nome_cidade);
-    printf("População total:\n", populacao);
-    printf("Área total:\n", area);
-    printf("PIB total:\n", pib);
-    printf("Total de pontos turísticos:\n", pts_turisticos);
+    printf("Código da cidade: %i\n", cod_cidade);
+    printf("Nome da cidade: %s\n", nome_cidade);
+    printf("População total: %i\n", populacao);
+    printf("Área total: %.2lf²\n", area);
+    printf("PIB total: %.2lf\n", pib);
+    printf("Total de pontos turísticos: %i\n", pts_turisticos);
 
     return 0;
 }
